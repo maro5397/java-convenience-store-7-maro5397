a922f1a2d600af77318eb0e70e5a86f3d58f84cf
reindeer002 <lsj000424@gmail.com>
2024-11-06 04:09:32 +0900

    refactor(product): change integer to big integer and add validation code for each field

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,35 +1,71 @@
 package store.domain;
 
+import java.math.BigInteger;
+
 public class Product {
     private final String name;
-    private final int price;
-    private final int stock;
-    private final String promotionStock;
+    private final BigInteger price;
+    private final BigInteger stock;
+    private final BigInteger promotionStock;
 
-    public Product(String name, int price, int stock, String promotionStock) {
+    public Product(String name, int price, int stock, int promotionStock) {
         this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.promotionStock = promotionStock;
+        this.price = BigInteger.valueOf(price);
+        this.stock = BigInteger.valueOf(stock);
+        this.promotionStock = BigInteger.valueOf(promotionStock);
+        validate(this);
     }
 
     public String getName() {
         return name;
     }
 
-    public int getPrice() {
+    public BigInteger getPrice() {
         return price;
     }
 
-    public int getStock() {
+    public BigInteger getStock() {
         return stock;
     }
 
-    public String getPromotionStock() {
+    public BigInteger getPromotionStock() {
         return promotionStock;
     }
 
     public boolean hasSufficientStock(int quantity) {
-        return stock - quantity >= 0;
+        BigInteger totalStock = stock.add(promotionStock);
+        BigInteger quantityBigInteger = BigInteger.valueOf(quantity);
+        return totalStock.compareTo(quantityBigInteger) >= 0;
+    }
+
+    private void validate(Product product) {
+        validateName(product.getName());
+        validatePrice(product.getPrice());
+        validateStock(product.getStock());
+        validateStock(product.getPromotionStock());
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 상품 이름이 없습니다.");
+        }
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 이름은 공백 문자열이 될 수 없습니다.");
+        }
+        if (name.length() > 100) {
+            throw new IllegalArgumentException("[ERROR] 상품 이름은 100자 이하여야 합니다.");
+        }
+    }
+
+    private void validatePrice(BigInteger price) {
+        if (price.compareTo(BigInteger.ZERO) <= 0) {
+            throw new IllegalArgumentException("[ERROR] 가격은 음수나 0이 될 수 없습니다.");
+        }
+    }
+
+    private void validateStock(BigInteger stock) {
+        if (stock.compareTo(BigInteger.ZERO) < 0) {
+            throw new IllegalArgumentException("[ERROR] 재고는 음수가 될 수 없습니다.");
+        }
     }
 }
