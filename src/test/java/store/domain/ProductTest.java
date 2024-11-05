17d174257401fd86ccd9e36bf9a53bdc02e1de47
reindeer002 <lsj000424@gmail.com>
2024-11-06 02:40:55 +0900

    test(product): add sufficient stock compare with purchase quantity

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ProductTest {
     @DisplayName("상품의 기본 정보 관리")
@@ -24,6 +25,28 @@ class ProductTest {
                     .isEqualTo(stock);
             softly.assertThat(product.getPromotionStock())
                     .isEqualTo(promotionStock);
+        });
+    }
+
+    @DisplayName("구매 수량이 재고 수량을 초과하지 않을 경우")
+    @ParameterizedTest(name = "구매 수량({0}), 재고 수량(10)")
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10})
+    void testHasSufficientStock(int quantity) {
+        Product product = new Product("콜라", 1000, 10, null);
+        boolean isSufficientStock = product.hasSufficientStock(quantity);
+        assertSoftly(softly -> {
+            softly.assertThat(isSufficientStock).isTrue();
+        });
+    }
+
+    @DisplayName("구매 수량이 재고 수량을 초과할 경우")
+    @ParameterizedTest(name = "구매 수량({0}), 재고 수량(10)")
+    @ValueSource(ints = {11,12,13,14,15,16,17,18,19,20})
+    void testHasNotSufficientStock(int quantity) {
+        Product product = new Product("콜라", 1000, 10, null);
+        boolean isSufficientStock = product.hasSufficientStock(quantity);
+        assertSoftly(softly -> {
+            softly.assertThat(isSufficientStock).isFalse();
         });
     }
 }
