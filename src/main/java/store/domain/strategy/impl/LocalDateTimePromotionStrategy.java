2d07bdafd1e662cdd9d1ebec4efe4c0c7f64f45a
reindeer002 <lsj000424@gmail.com>
2024-11-07 01:12:01 +0900

    refactor(strategy): change from localdatetime to string for easy test

--- src/main/java/store/domain/strategy/impl/LocalDateTimePromotionStrategy.java
+++ src/main/java/store/domain/strategy/impl/LocalDateTimePromotionStrategy.java
@@ -2,15 +2,17 @@ package store.domain.strategy.impl;
 
 import camp.nextstep.edu.missionutils.DateTimes;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import store.domain.strategy.PromotionStrategy;
 
 public class LocalDateTimePromotionStrategy implements PromotionStrategy {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     private final LocalDateTime startDate;
     private final LocalDateTime endDate;
 
-    public LocalDateTimePromotionStrategy(final LocalDateTime startDate, final LocalDateTime endDate) {
-        this.startDate = startDate;
-        this.endDate = endDate;
+    public LocalDateTimePromotionStrategy(String startDate, String endDate) {
+        this.startDate = LocalDateTime.parse(startDate, formatter);;
+        this.endDate = LocalDateTime.parse(endDate, formatter);;
     }
 
     @Override
