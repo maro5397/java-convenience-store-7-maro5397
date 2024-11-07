2e42429b030ada7ba0e6df59181277dd6ebdcaf5
reindeer002 <lsj000424@gmail.com>
2024-11-08 01:30:13 +0900

    feat(orders): implements get total price, promotion discount, membership discount specific

--- src/main/java/store/domain/Orders.java
+++ src/main/java/store/domain/Orders.java
@@ -9,14 +9,17 @@ public class Orders {
     public void addOrder(Product product, int quantity) {
         Order order = new Order(product, quantity);
         order.applyPromotionDiscount();
-        orders.add(new Order(product, quantity));
+        orders.add(order);
     }
 
     public int getTotalPrice() {
         int total = 0;
         for (Order order : orders) {
-            total += (order.getPromotionResult().getFreeItemCount() + order.getPromotionResult().getPaidItemCount()) * order.getProduct().getPromotionProduct().getPrice();
-            total += order.getPromotionResult().getNoneDiscountItemCount() * order.getProduct().getPrice();
+            if (order.getPromotionResult().getPromotionProductConsumeCount() != 0) {
+                total += order.getPromotionResult().getPromotionProductConsumeCount() * order.getProduct()
+                        .getPromotionProduct().getPrice();
+            }
+            total += order.getPromotionResult().getProductConsumeCount() * order.getProduct().getPrice();
         }
         return total;
     }
@@ -24,8 +27,11 @@ public class Orders {
     public int getPromotionDiscount() {
         int discountPrice = 0;
         for (Order order : orders) {
-            int freeItemCount = order.getPromotionResult().getFreeItemCount();
-            discountPrice += freeItemCount * order.getProduct().getPromotionProduct().getPrice();
+            if (order.getPromotionResult().getPromotionProductConsumeCount() != 0) {
+                discountPrice +=
+                        order.getPromotionResult().getFreeItemCount() * order.getProduct().getPromotionProduct()
+                                .getPrice();
+            }
         }
         return discountPrice;
     }
@@ -33,15 +39,18 @@ public class Orders {
     public int getMembershipDiscount() {
         int discountPrice = 0;
         for (Order order : orders) {
-            int noneDiscountItemCount = order.getPromotionResult().getNoneDiscountItemCount();
-            discountPrice += noneDiscountItemCount * order.getProduct().getPrice();
+            if(order.getPromotionResult().getPromotionProductConsumeCount() != 0) {
+                int noneDiscountPromotionStockCount = order.getPromotionResult().getNoneDiscountPromotionStockCount();
+                discountPrice += noneDiscountPromotionStockCount * order.getProduct().getPromotionProduct().getPrice();
+            }
+            discountPrice += order.getPromotionResult().getProductConsumeCount() * order.getProduct().getPrice();
         }
         return Math.min((int) (discountPrice * 0.3), 8000);
     }
 
     public Order getOrderByProductName(String name) {
         for (Order order : orders) {
-            if(order.getProduct().getName().equals(name)) {
+            if (order.getProduct().getName().equals(name)) {
                 return order;
             }
         }
