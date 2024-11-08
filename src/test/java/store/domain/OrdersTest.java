ac00a10e08c199a2630ee997e620dd47efa57d6a
reindeer002 <lsj000424@gmail.com>
2024-11-09 01:33:06 +0900

    test(orders): implements orders test to fit in requirements
    - add consume stock test functions for single responsibility
    - add repository to get test product and test promotion
    - add setting orders function for reduce duplicate

--- src/test/java/store/domain/OrdersTest.java
+++ src/test/java/store/domain/OrdersTest.java
@@ -2,64 +2,40 @@ package store.domain;
 
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import store.domain.strategy.impl.LocalDateTimePromotionStrategy;
+import org.junit.jupiter.params.provider.ValueSource;
+import store.repository.ProductRepository;
+import store.repository.PromotionRepository;
 
 class OrdersTest {
-    private final Map<String, Promotion> promotions = new HashMap<>();
-    private final Map<String, Product> products = new HashMap<>();
+    private ProductRepository productRepository;
+    private PromotionRepository promotionRepository;
+    private Orders orders;
 
     @BeforeEach
     void setUp() {
-        promotions.put("탄산2+1",
-                new Promotion("탄산2+1", 2, 1, new LocalDateTimePromotionStrategy("2024-01-01", "2024-12-31")));
-        promotions.put("MD추천상품",
-                new Promotion("MD추천상품", 1, 1, new LocalDateTimePromotionStrategy("2024-01-01", "2024-12-31")));
-        promotions.put("반짝할인",
-                new Promotion("반짝할인", 1, 1, new LocalDateTimePromotionStrategy("2024-11-01", "2024-11-30")));
-
-        products.put("콜라",
-                new Product("콜라", 1000, 10, new PromotionProduct("콜라", 1000, 10, promotions.get("탄산2+1"))));
-
-        products.put("사이다",
-                new Product("사이다", 1000, 8, new PromotionProduct("사이다", 1000, 8, promotions.get("탄산2+1"))));
-
-        products.put("오렌지주스",
-                new Product("오렌지주스", 1800, 0, new PromotionProduct("오렌지주스", 1800, 9, promotions.get("MD추천상품"))));
-
-        products.put("탄산수",
-                new Product("탄산수", 1200, 0, new PromotionProduct("탄산수", 1200, 5, promotions.get("탄산2+1"))));
-
-        products.put("물",
-                new Product("물", 500, 10, null));
-
-        products.put("비타민워터",
-                new Product("비타민워터", 1500, 6, null));
-
-        products.put("감자칩",
-                new Product("감자칩", 1500, 5, new PromotionProduct("감자칩", 1500, 5, promotions.get("반짝할인"))));
-
-        products.put("초코바",
-                new Product("초코바", 1200, 5, new PromotionProduct("초코바", 1200, 5, promotions.get("MD추천상품"))));
-
-        products.put("에너지바",
-                new Product("에너지바", 2000, 5, null));
-
-        products.put("정식도시락",
-                new Product("정식도시락", 6400, 8, null));
-
-        products.put("컵라면",
-                new Product("컵라면", 1700, 10, new PromotionProduct("컵라면", 1700, 1, promotions.get("MD추천상품"))));
+        this.productRepository = new ProductRepository("src/main/resources/products.md");
+        this.promotionRepository = new PromotionRepository("src/main/resources/promotions.md");
+        this.orders = new Orders();
     }
 
+    void settingOrders(String productName1, int quantity1, String productName2, int quantity2) {
+        Product product1 = this.productRepository.getProductWithName(productName1);
+        Product product2 = this.productRepository.getProductWithName(productName2);
+        this.orders.addOrder(product1, this.promotionRepository.getPromotionWithName(product1.getPromotion()),
+                quantity1);
+        this.orders.addOrder(product2, this.promotionRepository.getPromotionWithName(product2.getPromotion()),
+                quantity2);
+        for (Order order : this.orders.getOrders()) {
+            order.consumePromotionProduct();
+        }
+    }
 
-    @DisplayName("고객이 구매 요청한 상품을 목록에 추가")
-    @ParameterizedTest(name = "상품이름: {0}, 구매수량: {1}, 상품이름: {2}, 구매수량: {3}")
+    @DisplayName("고객이 구매 요청한 상품을 주문목록에 추가")
+    @ParameterizedTest(name = "상품이름1: {0}, 구매수량1: {1}, 상품이름2: {2}, 구매수량2: {3}")
     @CsvSource(
             value = {
                     "콜라,10,사이다,8",
@@ -67,13 +43,10 @@ class OrdersTest {
                     "컵라면,1,에너지바,3"
             }
     )
-    void testProductFieldManage(String productName1, int quantity1, String productName2, int quantity2) {
-        Orders orders = new Orders();
-        orders.addOrder(products.get(productName1), quantity1);
-        orders.addOrder(products.get(productName2), quantity2);
-
-        Order orderByProductName1 = orders.getOrderByProductName(productName1);
-        Order orderByProductName2 = orders.getOrderByProductName(productName2);
+    void testAddProductInOrders(String productName1, int quantity1, String productName2, int quantity2) {
+        settingOrders(productName1, quantity1, productName2, quantity2);
+        Order orderByProductName1 = this.orders.getOrders().getFirst();
+        Order orderByProductName2 = this.orders.getOrders().getLast();
         assertSoftly(softly -> {
             softly.assertThat(orderByProductName1.getProduct().getName()).isEqualTo(productName1);
             softly.assertThat(orderByProductName1.getQuantity()).isEqualTo(quantity1);
@@ -83,7 +56,7 @@ class OrdersTest {
     }
 
     @DisplayName("고객이 구매 요청한 상품 총액")
-    @ParameterizedTest(name = "상품이름: {0}, 구매수량: {1}, 상품이름: {2}, 구매수량: {3}, 총액: {4}")
+    @ParameterizedTest(name = "상품이름1: {0}, 구매수량1: {1}, 상품이름2: {2}, 구매수량2: {3}, 총액: {4}")
     @CsvSource(
             value = {
                     "컵라면,1,에너지바,3,7700",
@@ -92,16 +65,14 @@ class OrdersTest {
             }
     )
     void testTotalPrice(String productName1, int quantity1, String productName2, int quantity2, int total) {
-        Orders orders = new Orders();
-        orders.addOrder(products.get(productName1), quantity1);
-        orders.addOrder(products.get(productName2), quantity2);
+        settingOrders(productName1, quantity1, productName2, quantity2);
         assertSoftly(softly -> {
-            softly.assertThat(orders.getTotalPrice()).isEqualTo(total);
+            softly.assertThat(this.orders.getTotalPrice()).isEqualTo(total);
         });
     }
 
     @DisplayName("고객이 구매 요청한 상품의 프로모션 할인 총액")
-    @ParameterizedTest(name = "상품이름: {0}, 구매수량: {1}, 상품이름: {2}, 구매수량: {3}, 할인 총액: {4}")
+    @ParameterizedTest(name = "상품이름1: {0}, 구매수량1: {1}, 상품이름2: {2}, 구매수량2: {3}, 할인 총액: {4}")
     @CsvSource(
             value = {
                     "콜라,10,사이다,8,5000",
@@ -110,16 +81,14 @@ class OrdersTest {
             }
     )
     void testPromotionDiscount(String productName1, int quantity1, String productName2, int quantity2, int discount) {
-        Orders orders = new Orders();
-        orders.addOrder(products.get(productName1), quantity1);
-        orders.addOrder(products.get(productName2), quantity2);
+        settingOrders(productName1, quantity1, productName2, quantity2);
         assertSoftly(softly -> {
-            softly.assertThat(orders.getPromotionDiscount()).isEqualTo(discount);
+            softly.assertThat(this.orders.getPromotionDiscount()).isEqualTo(discount);
         });
     }
 
     @DisplayName("고객이 구매 요청한 상품의 맴버쉽 할인 총액")
-    @ParameterizedTest(name = "상품이름: {0}, 구매수량: {1}, 상품이름: {2}, 구매수량: {3}, 할인 총액: {4}")
+    @ParameterizedTest(name = "상품이름1: {0}, 구매수량1: {1}, 상품이름2: {2}, 구매수량2: {3}, 할인 총액: {4}")
     @CsvSource(
             value = {
                     "정식도시락,8,콜라,20,8000",
@@ -128,11 +97,46 @@ class OrdersTest {
             }
     )
     void testMembershipDiscount(String productName1, int quantity1, String productName2, int quantity2, int discount) {
-        Orders orders = new Orders();
-        orders.addOrder(products.get(productName1), quantity1);
-        orders.addOrder(products.get(productName2), quantity2);
+        settingOrders(productName1, quantity1, productName2, quantity2);
         assertSoftly(softly -> {
-            softly.assertThat(orders.getMembershipDiscount()).isEqualTo(discount);
+            softly.assertThat(this.orders.getMembershipDiscount(true)).isEqualTo(discount);
+            softly.assertThat(this.orders.getMembershipDiscount(false)).isZero();
+        });
+    }
+
+    @DisplayName("구매된 수량만큼 재고를 차감")
+    @ParameterizedTest(name = "구매 수량({0}), 재고 수량(10)")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void testDecrementStock(int quantity) {
+        Product product = this.productRepository.getProductWithName("감자칩");
+        this.orders.addOrder(product, this.promotionRepository.getPromotionWithName(product.getPromotion()),
+                quantity);
+        Order order = this.orders.getOrders().getFirst();
+        order.consumePromotionProduct();
+        order.applyConsumeStock();
+        assertSoftly(softly -> {
+            softly.assertThat(order.getProduct().getStock() + order.getProduct().getPromotionStock())
+                    .isEqualTo(10 - quantity);
+        });
+    }
+
+    @DisplayName("프로모션용 재고를 우선적으로 차감")
+    @ParameterizedTest(name = "구매 수량({0}), 일반 재고 수량({1}), 프로모션 재고 수량({2})")
+    @CsvSource(
+            value = {
+                    "1,5,4", "2,5,3", "3,5,2", "4,5,1", "5,5,0", "6,4,0", "7,3,0", "8,2,0", "9,1,0", "10,0,0"
+            }
+    )
+    void testDecrementPromotionStock(int quantity, int productStock, int promotionProductStock) {
+        Product product = this.productRepository.getProductWithName("감자칩");
+        this.orders.addOrder(product, this.promotionRepository.getPromotionWithName(product.getPromotion()),
+                quantity);
+        Order order = this.orders.getOrders().getFirst();
+        order.consumePromotionProduct();
+        order.applyConsumeStock();
+        assertSoftly(softly -> {
+            softly.assertThat(order.getProduct().getStock()).isEqualTo(productStock);
+            softly.assertThat(order.getProduct().getPromotionStock()).isEqualTo(promotionProductStock);
         });
     }
 }
