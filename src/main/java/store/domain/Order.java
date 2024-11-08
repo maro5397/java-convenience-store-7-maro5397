fc7bed2659d4e7becfd34169ba4e7b2287bedc57
reindeer002 <lsj000424@gmail.com>
2024-11-09 01:21:17 +0900

    feat(order): implements order to fit in requirements
    - erase and modify many functions for single responsibility
    - move responsibility to service object

--- src/main/java/store/domain/Order.java
+++ src/main/java/store/domain/Order.java
@@ -2,27 +2,81 @@ package store.domain;
 
 public class Order {
     private final Product product;
-    private final int quantity;
-    private PromotionResult promotionResult;
+    private final Promotion promotion;
+    private boolean canApplyAdditionalPromotion;
+    private OrderResult orderResult;
+    private int quantity;
 
-    public Order(Product product, int quantity) {
+    public Order(Product product, Promotion promotion, int quantity) {
         this.product = product;
+        this.promotion = promotion;
         this.quantity = quantity;
-    }
-
-    public void applyPromotionDiscount() {
-        promotionResult = product.decrementStock(quantity);
     }
 
     public Product getProduct() {
         return product;
     }
 
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    public OrderResult getOrderResult() {
+        return orderResult;
+    }
+
     public int getQuantity() {
         return quantity;
     }
 
-    public PromotionResult getPromotionResult() {
-        return promotionResult;
+    public boolean getCanApplyAdditionalPromotion() {
+        return canApplyAdditionalPromotion;
+    }
+
+    public OrderResult consumePromotionProduct() {
+        if (this.product.getPromotion().isEmpty()) {
+            OrderResult productOrderResult = new OrderResult(0, 0, 0, quantity);
+            this.orderResult = productOrderResult;
+            return productOrderResult;
+        }
+        OrderResult orderResult = this.promotion.calculatePromotionDiscount(this.product.getPromotionStock(),
+                this.quantity);
+        int productConsumeCount = orderResult.getProductConsumeCount();
+        int promotionProductConsumeCount = orderResult.getPromotionProductConsumeCount();
+        for (int i = 0;
+             i < orderResult.getProductConsumeCount()
+                     && this.product.getPromotionStock() > promotionProductConsumeCount;
+             i++) {
+            promotionProductConsumeCount += 1;
+            productConsumeCount -= 1;
+        }
+        this.orderResult = new OrderResult(orderResult.getPromotionApplyfreeItemCount(),
+                orderResult.getPromotionApplypaidItemCount(),
+                promotionProductConsumeCount, productConsumeCount);
+        canGetAdditionalProductByPromotion();
+        return this.orderResult;
+    }
+
+    public void applyAdditionalPromotion() {
+        quantity += promotion.getGet();
+    }
+
+    public void deleteNonePromotionAppliedProductCount() {
+        quantity -= this.orderResult.getPromotionProductConsumeCount()
+                + this.orderResult.getProductConsumeCount()
+                - this.orderResult.getPromotionApplyfreeItemCount()
+                - this.orderResult.getPromotionApplypaidItemCount();
+    }
+
+    public void applyConsumeStock() {
+        if (!this.product.getPromotion().isEmpty()) {
+            this.product.decrementPromotionStock(this.orderResult.getPromotionProductConsumeCount());
+        }
+        this.product.decrementStock(this.orderResult.getProductConsumeCount());
+    }
+
+    private void canGetAdditionalProductByPromotion() {
+        this.canApplyAdditionalPromotion = this.promotion.canApplyPromotion(quantity,
+                this.product.getPromotionStock() - this.orderResult.getPromotionProductConsumeCount());
     }
 }
