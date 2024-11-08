2a591ed361ab49346cb76d051073b4b78d2e604e
reindeer002 <lsj000424@gmail.com>
2024-11-09 02:19:39 +0900

    refactor(controller): divide start function into small function that perform for one purpose

--- src/main/java/store/controller/PurchaseController.java
+++ src/main/java/store/controller/PurchaseController.java
@@ -1,12 +1,9 @@
 package store.controller;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.concurrent.Callable;
 import store.domain.Order;
-import store.domain.OrderResult;
 import store.domain.Orders;
-import store.domain.Product;
 import store.service.PurchaseService;
 import store.view.InputView;
 import store.view.OutputView;
@@ -23,37 +20,69 @@ public class PurchaseController {
     }
 
     public void start() {
-        boolean retry = false;
         do {
-            this.outputView.displayGreeting();
-            List<Product> stock = this.purchaseService.getStock();
-            this.outputView.displayStockStatus(stock);
-            String purchaseInput = this.inputView.getPurchaseInput();
-            Orders orders = this.purchaseService.makeOrders(purchaseInput);
-            for (Order order : orders.getOrders()) {
-                this.purchaseService.applyPromotionProduct(order);
-                if (order.getCanApplyAdditionalPromotion()) {
-                    boolean confirmationFreeAdditionInput = this.inputView.getConfirmationFreeAdditionInput(order);
-                    if (confirmationFreeAdditionInput) {
-                        order.applyAdditionalPromotion();
-                    }
-                }
+            displayStockStatus();
+            Orders orders = getOrderFromCustomer();
+            findAdditionalPromotionDiscount(orders);
+            findNonePromotionDiscount(orders);
+            displayReceipt(orders);
+            applyConsumeStock(orders);
+        } while (askAdditionalPurchase());
+    }
+
+    private void displayStockStatus() {
+        this.outputView.displayGreeting();
+        this.outputView.displayStockStatus(this.purchaseService.getStock());
+    }
+
+    private Orders getOrderFromCustomer() {
+        return this.purchaseService.makeOrders(this.inputView.getPurchaseInput());
+    }
+
+    private void findAdditionalPromotionDiscount(Orders orders) {
+        for (Order order : orders.getOrders()) {
+            if (order.getCanApplyAdditionalPromotion()) {
+                suggestAdditionalPromotionDiscount(order);
             }
-            for (Order order : orders.getOrders()) {
-                OrderResult orderResult = this.purchaseService.applyPromotionProduct(order);
-                if (order.getPromotion() != null && orderResult.getNoneDiscountPromotionStockCount() != 0) {
-                    boolean confirmationNonePromotionInput = this.inputView.getConfirmationNonePromotionInput(order);
-                    if (!confirmationNonePromotionInput) {
-                        order.deleteNonePromotionAppliedProductCount();
-                    }
-                }
-                this.purchaseService.applyPromotionProduct(order);
+        }
+    }
+
+    private void suggestAdditionalPromotionDiscount(Order order) {
+        boolean confirmationFreeAdditionInput = this.inputView.getConfirmationFreeAdditionInput(order);
+        if (confirmationFreeAdditionInput) {
+            order.applyAdditionalPromotion();
+        }
+    }
+
+    private void findNonePromotionDiscount(Orders orders) {
+        for (Order order : orders.getOrders()) {
+            if (order.getPromotion() != null && order.getOrderResult().getNoneDiscountPromotionStockCount() != 0) {
+                suggestNonePromotionDiscount(order);
             }
-            boolean confirmationMembershipDiscountInput = this.inputView.getConfirmationMembershipDiscountInput();
-            this.outputView.displayReceipt(orders, confirmationMembershipDiscountInput);
-            orders.applyConsumeStock();
-            retry = this.inputView.getAdditionalPurchaseInput();
-        } while (retry);
+        }
+    }
+
+    private void suggestNonePromotionDiscount(Order order) {
+        boolean confirmationNonePromotionInput = this.inputView.getConfirmationNonePromotionInput(order);
+        if (!confirmationNonePromotionInput) {
+            order.deleteNonePromotionAppliedProductCount();
+        }
+    }
+
+    private boolean askMembershipDiscount() {
+        return this.inputView.getConfirmationMembershipDiscountInput();
+    }
+
+    private void displayReceipt(Orders orders) {
+        this.outputView.displayReceipt(orders, askMembershipDiscount());
+    }
+
+    private boolean askAdditionalPurchase() {
+        return this.inputView.getAdditionalPurchaseInput();
+    }
+
+    private void applyConsumeStock(Orders orders) {
+        orders.applyConsumeStock();
     }
 
     private <T> T executeWithRetry(Callable<T> callable) {
