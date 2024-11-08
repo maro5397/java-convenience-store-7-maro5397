462d1aa849e8f2f20b91920e2f39130919df2612
reindeer002 <lsj000424@gmail.com>
2024-11-09 01:04:57 +0900

    refactor(order-result): change name promotion-result to order-result

--- src/test/java/store/domain/OrderResultTest.java
+++ src/test/java/store/domain/OrderResultTest.java
@@ -6,23 +6,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class PromotionResultTest {
-    @DisplayName("영수증 정상 생성 테스트")
-    @ParameterizedTest(name = "프로모션 무료 상품 개수: {0}, 프로모션 유료 상품 개수: {1}, 일반 상품 개수: {2}")
+class OrderResultTest {
+    @DisplayName("주문 결과 정상 생성 테스트")
+    @ParameterizedTest(name = "프로모션 무료 상품 개수: {0}, 프로모션 유료 상품 개수: {1}, 상품 주문 개수: {2}")
     @CsvSource(
             value = {"10,11,12", "13,14,15"}
     )
     void testProductFieldManage(int freeItemCount, int paidItemCount, int quantity) {
-        OrderResult promotionResult = new OrderResult(freeItemCount, paidItemCount,
+        OrderResult orderResult = new OrderResult(freeItemCount, paidItemCount,
                 freeItemCount + paidItemCount, quantity - (freeItemCount + paidItemCount));
         assertSoftly(softly -> {
-            softly.assertThat(promotionResult.getPromotionApplyfreeItemCount())
+            softly.assertThat(orderResult.getPromotionApplyfreeItemCount())
                     .isEqualTo(freeItemCount);
-            softly.assertThat(promotionResult.getPromotionApplypaidItemCount())
+            softly.assertThat(orderResult.getPromotionApplypaidItemCount())
                     .isEqualTo(paidItemCount);
-            softly.assertThat(promotionResult.getPromotionProductConsumeCount())
+            softly.assertThat(orderResult.getPromotionProductConsumeCount())
                     .isEqualTo(freeItemCount + paidItemCount);
-            softly.assertThat(promotionResult.getProductConsumeCount())
+            softly.assertThat(orderResult.getProductConsumeCount())
                     .isEqualTo(quantity - (freeItemCount + paidItemCount));
         });
     }
