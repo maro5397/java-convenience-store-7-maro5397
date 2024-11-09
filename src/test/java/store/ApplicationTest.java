3cf73e2f1c29e30891c5cd991c94374673b62c01
reindeer002 <lsj000424@gmail.com>
2024-11-10 04:03:31 +0900

    test(main): add input not exist product name to make order test

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertNowTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -72,6 +74,16 @@ class ApplicationTest extends NsTest {
                     "현재 콜라 4개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)",
                     "현재 오렌지주스은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"
             );
+        });
+    }
+
+    @ParameterizedTest(name = "입력 주문: {0}")
+    @ValueSource(strings = {"[abc-12]", "[테스트-7]", "[솜사탕-5]"})
+    @DisplayName("존재하지 않는 상품 입력")
+    void testNotExistedProduct(String input) {
+        assertSimpleTest(() -> {
+            runException(input);
+            assertThat(output()).contains("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
         });
     }
 
