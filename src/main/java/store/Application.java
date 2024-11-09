99f8d8d55efd7e4a3de5868d7d166984b1699093
reindeer002 <lsj000424@gmail.com>
2024-11-09 19:12:06 +0900

    feat(main): add controller to start test of main

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,23 @@
 package store;
 
+import store.controller.PurchaseController;
+import store.repository.ProductRepository;
+import store.repository.PromotionRepository;
+import store.service.PurchaseService;
+import store.service.impl.PurchaseServiceImpl;
+import store.view.InputView;
+import store.view.OutputView;
+import store.view.impl.ConsoleInputViewImpl;
+import store.view.impl.ConsoleOutputViewImpl;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PromotionRepository promotionRepository = new PromotionRepository("src/main/resources/promotions.md");
+        ProductRepository productRepository = new ProductRepository("src/main/resources/products.md");
+        PurchaseService purchaseService = new PurchaseServiceImpl(productRepository, promotionRepository);
+        InputView inputView = new ConsoleInputViewImpl();
+        OutputView outputView = new ConsoleOutputViewImpl();
+        PurchaseController purchaseController = new PurchaseController(purchaseService, inputView, outputView);
+        purchaseController.start();
     }
 }
