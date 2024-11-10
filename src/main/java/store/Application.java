38744a706950c219e616ae1a579c16071402430c
reindeer002 <lsj000424@gmail.com>
2024-11-11 01:05:54 +0900

    refactor(main): use app config to get controller instance

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,23 +1,10 @@
 package store;
 
-import store.controller.PurchaseController;
-import store.repository.ProductRepository;
-import store.repository.PromotionRepository;
-import store.service.PurchaseService;
-import store.service.impl.PurchaseServiceImpl;
-import store.view.InputView;
-import store.view.OutputView;
-import store.view.impl.ConsoleInputViewImpl;
-import store.view.impl.ConsoleOutputViewImpl;
+import store.config.AppConfig;
 
 public class Application {
     public static void main(String[] args) {
-        PromotionRepository promotionRepository = new PromotionRepository("src/main/resources/promotions.md");
-        ProductRepository productRepository = new ProductRepository("src/main/resources/products.md");
-        PurchaseService purchaseService = new PurchaseServiceImpl(productRepository, promotionRepository);
-        InputView inputView = new ConsoleInputViewImpl();
-        OutputView outputView = new ConsoleOutputViewImpl();
-        PurchaseController purchaseController = new PurchaseController(purchaseService, inputView, outputView);
-        purchaseController.start();
+        AppConfig appConfig = new AppConfig();
+        appConfig.getPurchaseController().start();
     }
 }
